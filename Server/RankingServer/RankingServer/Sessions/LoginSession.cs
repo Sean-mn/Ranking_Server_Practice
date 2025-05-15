707ef957d67c9bb4137a4d74534d75a5073d8b55
Sean-mn <b01080080329@gmail.com>
2025-05-15 21:11:01 +0900

    LoginSession 구현

---
+++ Server/RankingServer/RankingServer/Sessions/LoginSession.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace RankingServer.Sessions;
+
+public class LoginSession : Session
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+
+    public override string ToJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+    public static LoginSession FromJson(string json)
+    {
+        return JsonSerializer.Deserialize<LoginSession>(json);
+    }
+}
