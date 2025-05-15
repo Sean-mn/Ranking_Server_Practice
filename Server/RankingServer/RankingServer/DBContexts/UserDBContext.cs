2ab9ebd5b8c3f1f198e819730542f191c5901688
Sean-mn <b01080080329@gmail.com>
2025-05-15 18:09:56 +0900

    UserDBContext 구현

---
+++ Server/RankingServer/RankingServer/DBContexts/UserDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RankingServer.Models;
+
+namespace RankingServer.DBContexts;
+
+public class UserDBContext : DbContext
+{
+    public UserDBContext(DbContextOptions<UserDBContext> options) : base(options) { }
+    
+    public DbSet<User> Users { get; set; }  
+}
