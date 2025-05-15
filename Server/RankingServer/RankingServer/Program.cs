5aa94b0b2a0596171394a39f0c3b4aad74c3a871
Sean-mn <b01080080329@gmail.com>
2025-05-15 21:11:53 +0900

    Program 세션 사용 기능 추가

--- Server/RankingServer/RankingServer/Program.cs
+++ Server/RankingServer/RankingServer/Program.cs
@@ -3,8 +3,6 @@ using RankingServer.DBContexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 if (string.IsNullOrEmpty(connectionString))
 {
@@ -15,6 +13,22 @@ builder.Services.AddDbContext<UserDBContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
+builder.Services.AddControllers();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(30);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    }
+    );
+
+var app = builder.Build();
+
+app.UseSession();
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
