1fb005e05b8e84501d6e2db79b38be9fac21c0ce
Sean-mn <b01080080329@gmail.com>
2025-05-15 19:42:58 +0900

    MySQL 설정

--- Server/RankingServer/RankingServer/Program.cs
+++ Server/RankingServer/RankingServer/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using RankingServer.DBContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("DB_CONNECTION is missing.");
+}
+
+builder.Services.AddDbContext<UserDBContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+
 
 app.UseAuthorization();
 
