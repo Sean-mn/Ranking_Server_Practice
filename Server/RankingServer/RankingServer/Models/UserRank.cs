8351c8e066bc9c425524b25c581b5416702a4b04
Sean-mn <b01080080329@gmail.com>
2025-05-20 11:18:08 +0900

    UserRank 모델 생성

---
+++ Server/RankingServer/RankingServer/Models/UserRank.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RankingServer.Models;
+
+[Table("userRank")]
+public class UserRank
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public int UserScore { get; set; }
+}
