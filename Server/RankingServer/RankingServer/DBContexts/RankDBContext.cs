533258953520b9fe90d114502a97cf9d949e29e9
Sean-mn <b01080080329@gmail.com>
2025-05-20 11:17:55 +0900

    RankDBContext 구현

---
+++ Server/RankingServer/RankingServer/DBContexts/RankDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RankingServer.Models;
+
+namespace RankingServer.DBContexts;
+
+public class RankDBContext : DbContext
+{
+    public DbSet<UserRank> UserRanks { get; set; }
+    
+    public RankDBContext(DbContextOptions<RankDBContext> options) : base(options) { }
+}
